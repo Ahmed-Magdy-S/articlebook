223c0b4297528953d30cda97ac4f5b4b5a6abf4c
Ahmed-Magdy-S <ahmed.magdy.s.19@gmail.com>
2024-01-07 12:31:56 +0200

    init project

---
+++ src/main/java/net/articlebook/ArticleBookApplication.java
@@ -0,0 +1,13 @@
+package net.articlebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticleBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArticleBookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/articlebook/ArticleBookApplicationTests.java
@@ -0,0 +1,13 @@
+package net.articlebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticleBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
